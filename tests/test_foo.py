a64b9f0b18c2dd69cd6876fbe347faa5c9ca1d8d
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-01-24 16:47:37 +0100

    [test] add module & submodule import tests

--- tests/test_foo.py
+++
@@ -1,5 +0,0 @@
-import atomsmltr as atm
-
-
-def test_import():
-    return
---
+++ tests/test_imports.py
@@ -0,0 +1,5 @@
+def test_import():
+    import atomsmltr as atm
+    from atomsmltr import atoms, datapipe, environment, simulation, utils
+
+    return
