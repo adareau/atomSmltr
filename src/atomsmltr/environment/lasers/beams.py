a3541faed7d71258e7492e7e3d11bcac434f13a6
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-01-27 17:18:49 +0100

    [WIP] working on laser beam class

---
+++ src/atomsmltr/environment/lasers/beams.py
@@ -0,0 +1,176 @@
+# -*- coding: utf-8 -*-
+"""Defines the laser beam classes
+"""
+
+# % IMPORTS
+import numpy as np
+import numpy.typing as npt
+from abc import ABC, abstractmethod
+from enum import Enum, auto
+
+# % GLOBAL DEFINITIONS
+
+DIRECTION_TYPES = ["vector", "thetaphi"]  # allowed values for `direction_type``
+
+
+# % TOOL FUNCTIONS
+
+
+def _intensity_gauss(
+    r: float, z: float, w0: float, P: float, wavelength: float
+) -> float:
+    """Computes intensity for a Gaussian beam of waist w0 and power P0 at position
+    (r, z), in cynlindrical coordinates. The beam is propagating along z, and the waist
+    is located at r = z = 0. Lengths should be given in meters, and powers in watts.
+    Intensity is returned in W/m^2
+
+    Args:
+        r (float): radial coordinate (distance to beam axis) in _meters_
+        z (float): axial coordinate (distance to beam waist) in _meters_
+        w0 (float): Gaussian beam waist radius (1/e^2) in _meters_
+        P (float): laser power in _Watts_
+        wavelength (float): laser wavelength in _meters_
+
+    Returns:
+        intensity (float): laser intensity in _W/m^2_
+    """
+
+    zR = np.pi * w0**2 / wavelength
+    wz = w0 * np.sqrt(1 + z**2 / zR**2)
+    I0 = 2 * P / np.pi / w0**2
+    intensity = I0 * (w0 / wz) ** 2 * np.exp(-2 * (r**2) / wz**2)
+    return intensity
+
+
+# % ABSTRACT CLASSES
+
+
+class AbstractLaserBeam(ABC):
+    """docstring for AbstractLaserBeam."""
+
+    def __init__(
+        self,
+        wavelength: float,
+        waist: float,
+        power: float,
+        waist_position: npt.ArrayLike,
+        direction: npt.ArrayLike,
+        direction_type: str = "vector",
+    ):
+        self.wavelength = wavelength
+        self.waist = waist
+        self.power = power
+        self.waist_position = waist_position
+        # /!\ direction_type has to be defined BEFORE direction !!
+        self.direction_type = direction_type
+        self.direction = direction
+
+        super().__init__()
+
+    # -- class properties setters & getters
+    # - wavelength
+    @property
+    def wavelength(self) -> float:
+        return self._wavelength
+
+    @wavelength.setter
+    def wavelength(self, value: float) -> None:
+        self._positive_float_check("wavelength", value)
+        if value > 3e-6 or value < 100e-9:
+            raise Warning(
+                "Value given for wavelength is outside the 100nm-3µm range, which is rather strange. Check that you have given the wavelength value in _meters_"
+            )
+        self._wavelength = float(value)
+
+    # - waist
+    @property
+    def waist(self) -> float:
+        return self._waist
+
+    @waist.setter
+    def waist(self, value: float) -> None:
+        self._positive_float_check("waist", value)
+        self._waist = float(value)
+
+    # - power
+    @property
+    def power(self) -> float:
+        return self._power
+
+    @power.setter
+    def power(self, value: float) -> None:
+        self._positive_float_check("power", value)
+        self._power = float(value)
+
+    # - waist position
+    @property
+    def waist_position(self) -> npt.ArrayLike:
+        return self._waist_position
+
+    @waist_position.setter
+    def waist_position(self, value: npt.ArrayLike) -> None:
+        value = np.asanyarray(value)
+        if value.size != 3:
+            raise ValueError("'waist_position' should be an array-like of size 3")
+        self._waist_position = value
+
+    # - direction_type
+    @property
+    def direction_type(self) -> str:
+        return self._direction_type
+
+    @direction_type.setter
+    def direction_type(self, value: str) -> None:
+        if value not in DIRECTION_TYPES:
+            raise ValueError(f"'direction_type' should be in {DIRECTION_TYPES}")
+        self._direction_type = value
+
+    # - direction
+    @property
+    def direction(self) -> npt.ArrayLike:
+        return self._direction
+
+    @direction.setter
+    def direction(self, value: npt.ArrayLike) -> None:
+        # convert to array
+        value = np.asanyarray(value)
+
+        # check that the size is OK
+        errormsg = "When 'direction_type' is set to '{direction_type}', 'direction' should be an array of size {size}"
+        if self.direction_type == "vector" and value.size != 3:
+            raise ValueError(errormsg.format(direction_type="vector", size=3))
+        elif self.direction_type == "thetaphi" and value.size != 2:
+            raise ValueError(errormsg.format(direction_type="thetaphi", size=2))
+
+        # for unit vector, normalize
+        if self.direction_type == "vector":
+            norm = np.sqrt(np.sum(value * value))
+            if norm == 0:
+                raise ValueError("Wrong value for the unit vector: norm is zero")
+            value = value / norm
+        self._direction = value
+
+    # -- hidden methods
+    def _positive_float_check(self, param_name: str, value: float) -> None:
+        """internal function to check that a parameter is a positive float, raises a `ValueError` if not.
+
+        Args:
+            param_name (str): name of the checked parameter, to give context in the exception
+            value (float): value of the paramater to check
+        """
+        if isinstance(value, int):
+            value = float(value)
+        if not isinstance(value, float):
+            raise ValueError(f"'{param_name}' has to be a float")
+        if value < 0:
+            raise ValueError(f"'{param_name}' has to be a positive")
+
+
+# % IMPLEMENTED CLASSES
+
+
+class GaussianLaserBeam(AbstractLaserBeam):
+    """docstring for GaussianLaserBeam."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ tests/test_laser_beam.py
@@ -0,0 +1,148 @@
+import pytest
+import numpy as np
+
+
+def test_Gaussian_laser_beam_properties():
+    from atomsmltr.environment.lasers.beams import GaussianLaserBeam
+
+    # - testing initialization
+    gaussian_beam = GaussianLaserBeam(
+        wavelength=780e-9,
+        waist=20e-6,
+        power=50e-3,
+        waist_position=[0, 0, 0],
+        direction=[0, 0, 1],
+        direction_type="vector",
+    )
+
+    # - testing setters and getters
+    # wavelength
+    new_wavelength = 781e-9
+    gaussian_beam.wavelength = new_wavelength
+    assert gaussian_beam.wavelength == new_wavelength
+
+    # waist
+    new_waist = 30e-6
+    gaussian_beam.waist = new_waist
+    assert gaussian_beam.waist == new_waist
+
+    # power
+    new_power = 60e-3
+    gaussian_beam.power = new_power
+    assert gaussian_beam.power == new_power
+
+    # waist_position > array
+    new_waist_position = np.array([1.0, 1.0, 1.0])
+    gaussian_beam.waist_position = new_waist_position
+    assert np.array_equal(gaussian_beam.waist_position, new_waist_position)
+
+    # waist_position > tuple
+    new_waist_position = (1.0, 1.0, 1.0)
+    gaussian_beam.waist_position = new_waist_position
+    assert np.array_equal(gaussian_beam.waist_position, new_waist_position)
+
+    # direction_type
+    new_direction_type = "thetaphi"
+    gaussian_beam.direction_type = new_direction_type
+    assert gaussian_beam.direction_type == new_direction_type
+
+    # direction > thetaphi
+    gaussian_beam.direction_type = "thetaphi"
+    new_direction = [0.5, 3.0]
+    gaussian_beam.direction = new_direction
+    assert np.array_equal(gaussian_beam.direction, new_direction)
+
+    # direction > vector
+    gaussian_beam.direction_type = "vector"
+    new_direction = [0.1, 0.1, 0.1]
+    gaussian_beam.direction = new_direction
+    new_direction_unit = np.asanyarray(new_direction)
+    new_direction_norm = np.sqrt(np.sum(new_direction_unit * new_direction_unit))
+    new_direction_unit /= new_direction_norm
+    assert np.array_equal(gaussian_beam.direction, new_direction_unit)
+
+
+def test_Gaussian_laser_beam_exception():
+    from atomsmltr.environment.lasers.beams import GaussianLaserBeam
+
+    # - init
+    gaussian_beam = GaussianLaserBeam(
+        wavelength=780e-9,
+        waist=20e-6,
+        power=50.0,
+        waist_position=[0, 0, 0],
+        direction=[0, 0, 1],
+        direction_type="vector",
+    )
+
+    # - wrong values for setters
+    # wavelength
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.wavelength = "451"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.wavelength = {}
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.wavelength = None
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.wavelength = [
+            1.5,
+        ]
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.wavelength = -45.4
+    with pytest.raises(Warning) as excinfo:
+        gaussian_beam.wavelength = 5.0
+
+    # waist
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist = "451"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist = {}
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist = None
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist = [
+            1.5,
+        ]
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist = -45.4
+
+    # power
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.power = "451"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.power = {}
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.power = None
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.power = [
+            1.5,
+        ]
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.power = -45.4
+
+    # waist position
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist_position = "451"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist_position = 0.5
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.waist_position = [0.5, 0.5]
+
+    # direction type
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.direction_type = "abc"
+
+    # direction type
+    gaussian_beam.direction_type = "vector"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.direction_type = [1, 1]
+    gaussian_beam.direction_type = "thetaphi"
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.direction_type = [1, 1, 1]
+    with pytest.raises(ValueError) as excinfo:
+        gaussian_beam.direction_type = [0, 0, 0]
+
+
+if __name__ == "__main__":
+    test_Gaussian_laser_beam_properties()
+    test_Gaussian_laser_beam_exception()
