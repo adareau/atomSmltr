4fdcf22321ecb3d2ea95aa6216b62c7056e640ed
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-02-12 18:13:33 +0100

    [plotter] factorizing argument check for 3D plot

--- src/atomsmltr/environment/lasers/beams.py
+++ src/atomsmltr/environment/lasers/beams.py
@@ -541,54 +541,10 @@ class AbstractLaserBeam(Plottable):
         Returns:
             ax (matplotlib axis): the axis
         """
-        # ------------------------- START ARGUMENT CHECKING ----------------
-        # - check plot config
-        IMPLEMENTED_PLANES = ["XY", "YZ", "ZX"]
-        if plane.upper() not in IMPLEMENTED_PLANES:
-            raise ValueError(f"`plane` argument should be in {IMPLEMENTED_PLANES}")
-
-        assert ax is None or isinstance(ax, Axes), "'ax' should be a matplotlib axis."
-        # - check axis config
-        # general
-        if (limits is not None) + (Npoints is not None) + (X is not None) + (
-            Y is not None
-        ) > 2:
-            msg = "Too many arguments given for meshgrid definition. "
-            msg += "Either provide `limits` and `Npoints` or `X` and `Y`"
-            raise ValueError(msg)
-        if (limits is None) + (Npoints is None) == 1:
-            raise ValueError("Both `limits` and `Npoints` arguments have to be passed")
-        if (X is None) + (Y is None) == 1:
-            raise ValueError("You have to provide both `X` and `Y` mesh")
-        # argument per argument
-        # limits
-        assert (
-            limits is None or np.asanyarray(limits).size == 4
-        ), "`limits` should be an array of size 4"
-        # Npoints
-        if Npoints is not None:
-            Npoints = np.asanyarray(Npoints)
-            msg = "`Npoints` should be an int or a list of two ints"
-            assert Npoints.size in [1, 2], msg
-            assert issubclass(Npoints.dtype.type, np.integer), msg
-        # X, Y
-        if X is not None:
-            X = np.asanyarray(X)
-            Y = np.asanyarray(Y)
-            assert X.shape == Y.shape, "'X' and 'Y' should have the same shape"
-        # ------------------------- STOP ARGUMENT CHECKING ----------------
-
-        # - init ax (if needed)
-        ax = self._init_ax(ax)
-
-        # - init meshgrid
-        if X is None:
-            xmin, xmax, ymin, ymax = limits
-            Nx, Ny = (Npoints, Npoints) if Npoints.size == 1 else Npoints
-            print(Nx)
-            x = np.linspace(xmin, xmax, Nx)
-            y = np.linspace(ymin, ymax, Ny)
-            X, Y = np.meshgrid(x, y)
+        # - process arguments using the Plottable builtin method
+        ax, X, Y = self._process_2D_plot_args(
+            ax=ax, plane=plane, limits=limits, Npoints=Npoints, X=X, Y=Y
+        )
 
         # - compute intensity
         match plane.upper():
