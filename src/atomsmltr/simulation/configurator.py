1a68793a7fbce0abb2ea6e22400948377156096d
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-02-14 12:06:07 +0100

    [configuration] start with collection handling

--- src/atomsmltr/simulation/configurator.py
+++ src/atomsmltr/simulation/configurator.py
@@ -5,6 +5,7 @@ to make a consistent configuration for the simulator
 
 # % LOCAL IMPORTS
 from ..environment import AbstractLaserBeam, MagneticField
+from ..environment.envbase import EnvObject
 from ..atoms import Atom
 
 
@@ -12,7 +13,7 @@ from ..atoms import Atom
 
 
 class Configuration(object):
-    def __init__(self, object_list=[], atom=None):
+    def __init__(self, object_list=None, atom=None):
         # - initialize collections
         self.__lasers = {}
         self.__zones = {}
@@ -24,7 +25,96 @@ class Configuration(object):
         if atom is not None:
             self.atom = atom
 
+        if object_list is not None:
+            self.add_objects(object_list)
+
     # -- COLLECTION HANDLING METHOD
+
+    # ADDING
+    def add_objects(self, obj: EnvObject | list, verbose=False):
+        # - check argument
+        type_err_msg = "passed argument should be an EnvObject or a list of EnvObjects"
+        if isinstance(obj, (list, tuple)):
+            for element in obj:
+                if not isinstance(element, EnvObject):
+                    raise TypeError(type_err_msg)
+        elif not isinstance(obj, EnvObject):
+            raise TypeError(type_err_msg)
+
+        # - recursive add if list
+        if isinstance(obj, (list, tuple)):
+            for element in obj:
+                self.add_objects(element, verbose)
+            return
+
+        # - add object
+        if isinstance(obj, MagneticField):
+            collection = self.__magfields
+            name = "magnetic fields"
+        elif isinstance(obj, AbstractLaserBeam):
+            collection = self.__lasers
+            name = "lasers"
+        else:
+            msg = f"Objects of type {type(obj)} are not handled yet.. where did you find this ?"
+            raise TypeError(msg)
+        self.__add_obj(obj, collection, name)
+
+        if verbose:
+            msg = f"(+) sucessfully added object '{obj.tag}' in the {name} collection"
+            print(msg)
+
+    def __add_obj(self, obj, collection, name):
+        """Internal method to add objects"""
+        # - check that object tag not present
+        msg = f"We already have an element with tag '{obj.tag}' in our {name} collection. "
+        msg += "Remove or update this element."
+        if obj.tag in collection:
+            raise ValueError(msg)
+        # - add the object
+        collection[obj.tag] = obj
+
+    # LISTING
+    def list_lasers(self):
+        return list(self.__lasers)
+
+    def list_magnetic_fields(self):
+        return list(self.__magfields)
+
+    # REMOVING
+    def rm_object(self, collection, tag):
+        implemented_collections = {
+            "laser": self.__lasers,
+            "magnetic field": self.__magfields,
+        }
+        if collection not in implemented_collections:
+            msg = f"Wrong collection. implemented collections are {list(implemented_collections)}"
+            raise ValueError(msg)
+
+        coll = implemented_collections[collection]
+        if tag not in coll:
+            msg = f"There is no {collection} with tag {tag}"
+            raise KeyError(msg)
+        del coll[tag]
+
+    def rm_laser(self, tag):
+        return self.rm_object("laser", tag)
+
+    def rm_magnetic_field(self, tag):
+        return self.rm_object("magnetic field", tag)
+
+    def rm_all_objects(self):
+        self.__lasers = {}
+        self.__magfields = {}
+        self.__zones = {}
+
+    def rm_all_lasers(self):
+        self.__lasers = {}
+
+    def rm_all_magnetic_fields(self):
+        self.__magfields = {}
+
+    def rm_all_zones(self):
+        self.__zones = {}
 
     # -- GETTERS & SETTERS
     @property
--- tests/simulation/test_configuration.py
+++ tests/simulation/test_configuration.py
@@ -1,6 +1,21 @@
 import pytest
 
 
+def _get_env_objects():
+    from atomsmltr.environment.fields.magnetic import MagneticOffset
+    from atomsmltr.environment.lasers.beams import GaussianLaserBeam
+
+    mag_field_1 = MagneticOffset((0, 1, 1), tag="offset1")
+    mag_field_2 = MagneticOffset((0, 0, 2), tag="offset2")
+    laser_1 = GaussianLaserBeam(
+        780e-9, 20e-6, 10e-3, (0, 0, 0), (0, 0, 1), tag="laser1"
+    )
+    laser_2 = GaussianLaserBeam(
+        780e-9, 20e-6, 10e-3, (0, 0, 0), (0, 0, 1), tag="laser2"
+    )
+    return mag_field_1, mag_field_2, laser_1, laser_2
+
+
 def test_configuration():
     from atomsmltr.simulation import Configuration
     from atomsmltr.atoms.collection import Ytterbium
@@ -11,14 +26,71 @@ def test_configuration():
     # -- set atom
     config.atom = Ytterbium()
 
+    # -- set environment
+    mag1, mag2, las1, las2 = _get_env_objects()
+    config.add_objects(mag1, verbose=True)
+    config.add_objects(las1, verbose=True)
+    config.add_objects([mag2, las2], verbose=True)
+
+    # -- listing and removing
+    for tag in config.list_lasers():
+        config.rm_object("laser", tag)
+    assert len(config.list_lasers()) == 0
+
+    for tag in config.list_magnetic_fields():
+        config.rm_object("magnetic field", tag)
+    assert len(config.list_magnetic_fields()) == 0
+
+    config.add_objects(mag1)
+    config.rm_magnetic_field("offset1")
+    assert len(config.list_magnetic_fields()) == 0
+
+    config.add_objects(las1)
+    config.rm_laser("laser1")
+    assert len(config.list_lasers()) == 0
+
+    config.add_objects([mag1, mag2, las1, las2])
+    config.rm_all_objects()
+    assert len(config.list_magnetic_fields()) == 0
+    assert len(config.list_lasers()) == 0
+
+    config.add_objects([mag1, mag2, las1, las2])
+    config.rm_all_magnetic_fields()
+    assert len(config.list_magnetic_fields()) == 0
+    assert len(config.list_lasers()) == 2
+    config.add_objects([mag1, mag2])
+    config.rm_all_lasers()
+    assert len(config.list_magnetic_fields()) == 2
+    assert len(config.list_lasers()) == 0
+
 
 def test_configuration_exceptions():
     from atomsmltr.simulation import Configuration
 
+    # -- init
     config = Configuration()
 
+    # check atom exception
     with pytest.raises(TypeError) as excinfo:
         config.atom = "ytterbium"
+
+    # check environement
+    mag1, mag2, las1, las2 = _get_env_objects()
+    # adding wrong types
+    with pytest.raises(TypeError) as excinfo:
+        config.add_objects("laser")
+    with pytest.raises(TypeError) as excinfo:
+        config.add_objects([mag1, mag2, "laser"])
+    config.add_objects(mag1)
+    # try to add object with already existing tag
+    with pytest.raises(ValueError) as excinfo:
+        config.add_objects(mag1)
+    # try to remove object from wrong collection
+    with pytest.raises(ValueError) as excinfo:
+        config.rm_object("MAgnetic fields", "mag1")
+    # try to add object with already existing tag
+    with pytest.raises(KeyError) as excinfo:
+        config.rm_laser("las3")
 
 
 if __name__ == "__main__":
