73a4c4ce78b32253784a87d3f7b0d831e3e07e0c
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-03-04 16:57:11 +0100

    [cleanup] atom package init

--- src/atomsmltr/atoms/__init__.py
+++ src/atomsmltr/atoms/__init__.py
@@ -1,2 +1,11 @@
+"""The `atomsmltr.atoms` subpackage provides classes to handle atomic species and their transitions.
+"""
+
+__all__ = [
+    "Atom",
+    "Ytterbium",
+    "Strontium",
+]
+
 from .generic import Atom
 from .collection import Ytterbium, Strontium
