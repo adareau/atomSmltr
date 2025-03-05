5c9d6caba6c1f88583dd7e3870e6792b4f8fb7e5
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-03-05 10:22:43 +0100

    [cleanup] environment.fields.__init__

--- src/atomsmltr/environment/fields/__init__.py
+++ src/atomsmltr/environment/fields/__init__.py
@@ -1,3 +1,26 @@
+"""The ``atomsmltr.environment.fields`` subpackage provides definitions for vector fields
+
+Currently, only magnetic fields are implemented, but this could be extented to electric fields
+
+Examples
+---------
+
+Setup a magnetic field offset
+
+>>> from atomsmltr.environment.fields import MagneticOffset
+>>> offset_field = MagneticOffset(offset=(0,1,0), tag="offset")
+
+"""
+
+__all__ = [
+    "MagneticGradient",
+    "MagneticOffset",
+    "MagneticQuadrupoleX",
+    "MagneticQuadrupoleY",
+    "MagneticQuadrupoleZ",
+    "InterpMag1D1D",
+]
+
 from .magnetic import (
     MagneticGradient,
     MagneticOffset,
