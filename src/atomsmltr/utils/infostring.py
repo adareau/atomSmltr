2d113f87f39e9a8bcb42cdbaf2b6a990d37d82f9
Alexandre Dareau <alexandre.Dareau@gmail.com>
2025-03-06 10:17:41 +0100

    [cleanup] utils, infostring

--- src/atomsmltr/utils/infostring.py
+++ src/atomsmltr/utils/infostring.py
@@ -1,6 +1,50 @@
-# -*- coding: utf-8 -*-
-"""tools to generate and display info strings
+"""infostrings
+==================
+
+Here we implement the ``InfoString`` class, that allows to gather
+different parameters from objects and to display them in a tree format.
+
+This is used in the ``print_info()`` method that is present in most
+classes accross the module.
+
+Examples
+--------------------
+
+Run a simulation with one initial condition vector
+
+.. code-block:: python
+
+        from atomsmltr.utils.infostring import InfoString
+
+        info = InfoString("PARAMETERS")
+        info.add_section("First Section")
+        info.add_element("elem", "param")
+        info.add_element("x", f"{25.4e-6:.2e}µm")
+        info.add_element("y", f"{15.8:.2f} mW")
+        info.add_section("Another section")
+        info.add_element("elem", "param")
+        info.add_element("elem2", "param2")
+
+        print(info.generate())
+
+This prints :
+
+.. code-block::
+
+        ──────────────
+        | PARAMETERS |
+        ──────────────
+        . First Section :
+        ├── elem : param
+        ├── x : 2.54e-05µm
+        └── y : 15.80 mW
+
+        . Another section :
+        ├── elem : param
+        └── elem2 : param2
+
 """
+
 # % IMPORTS
 
 from collections import OrderedDict
@@ -18,7 +62,13 @@ TITLE = "| {} |\n"
 
 
 class InfoString(object):
-    """Allows to generate info strings"""
+    """Generates info strings
+
+    Parameters
+    ----------
+    title : str
+        the title of the infostring object
+    """
 
     def __init__(self, title: str):
         self.__title = title
@@ -27,10 +77,12 @@ class InfoString(object):
 
     @property
     def elements(self):
+        """OrderedDict: the elements inside the infostring object"""
         return self.__elements
 
     @property
     def title(self) -> str:
+        """str: titile of the infostring object"""
         return self.title
 
     @title.setter
@@ -39,12 +91,31 @@ class InfoString(object):
         self.__title = value
 
     def add_section(self, name: str):
+        """adds a new section for parameters
+
+        Parameters
+        ----------
+        name : str
+            the section name
+        """
         if name in self.__elements:
             raise Warning(f"section '{name}' already exists")
         self.__elements[name] = OrderedDict()
         self.__current_section = name
 
-    def add_element(self, name: str, value: str = None, section=None):
+    def add_element(self, name: str, value: str = None, section: str = None):
+        """adds an element in a given section
+
+        Parameters
+        ----------
+        name : str
+            name of the element
+        value : str, optional
+            value of the element, by default None
+        section : str, optional
+            name of the section. If None is given, the element is added to
+            the lastly used section, by default None
+        """
         if section is None:
             section = self.__current_section
         if section not in self.__elements:
@@ -55,7 +126,18 @@ class InfoString(object):
             raise Warning(f"section '{section}' already has an element {name}")
         self.__elements[section][name] = value
 
-    def rm_element(self, name: str, section=None):
+    def rm_element(self, name: str, section: str = None):
+        """removes an element from a section
+
+        Parameters
+        ----------
+        name : str
+            name of the element to remove
+        section : str, optional
+            name of the section. If None is given, the element is removed from
+            the lastly used section, by default None
+        """
+
         if section is None:
             section = self.__current_section
         if section not in self.__elements:
@@ -66,8 +148,19 @@ class InfoString(object):
             raise Warning(f"section '{section}' does not have an element {name}")
         del self.__elements[section][name]
 
-    def absorb_section(self, info, target_section, new_name=None):
-        """incorporates the section 'section' from a info object 'info'"""
+    def absorb_section(self, info, target_section: str, new_name: str = None):
+        """incorporates a section from another infostring object
+
+        Parameters
+        ----------
+        info : InfoString
+            the infostring object from which we take the section
+        target_section : str
+            name of the section to incorporate
+        new_name : str, optional
+            name of the incorporated section.
+            If None is given we use the name of the original section, by default None
+        """
         # assert
         assert isinstance(info, InfoString), "'info' should be an InfoString object"
         # get info
@@ -83,7 +176,16 @@ class InfoString(object):
             self.add_element(name, value)
 
     def merge(self, info, prefix=""):
-        """Merges info strings"""
+        """merges an entire infostring
+
+        Parameters
+        ----------
+        info : InfoString
+            infostring object to merge
+        prefix : str, optional
+            prefix added to the names of the sections incorporated
+            from the merged infostring, by default ""
+        """
         # assert
         assert isinstance(info, InfoString), "'info' should be an InfoString object"
         # merge
@@ -93,6 +195,18 @@ class InfoString(object):
                 self.add_element(name, value)
 
     def generate(self, display_title=True):
+        """generates a string from the infostring object
+
+        Parameters
+        ----------
+        display_title : bool, optional
+            whether to display the title, by default True
+
+        Returns
+        -------
+        out_str: str
+            a string with all parameters from the info string
+        """
         # init
         out = []
         # title
