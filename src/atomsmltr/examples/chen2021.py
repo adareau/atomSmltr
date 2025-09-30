3a67d8c97020940eb4cc7525e9e23a9965b35724
Mateo Weill <mateo.weill@gmail.com>
2025-09-30 15:35:15 +0200

    first commit, black issue fixed

--- src/atomsmltr/examples/chen2021.py
+++ src/atomsmltr/examples/chen2021.py
@@ -36,25 +36,24 @@ mag_field = MagneticQuadrupoleZ(origin=origin, slope=gradient, tag="mag_field_1"
 # -- setup lasers of the 1D MOT
 # cf. config from Chen 2021
 laser_1_MOT = GaussianLaserBeam(
-    wavelength = 460.862e-9,
-    waist = (1e-2)*np.sqrt(2),
-    power = 3e-2,
-    waist_position = (0, 0, 0),
-    direction = (0, 0, 1),
-    polarization = CircularLeft(),
-    tag = "las1mot"    
+    wavelength=460.862e-9,
+    waist=(1e-2) * np.sqrt(2),
+    power=3e-2,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, 1),
+    polarization=CircularLeft(),
+    tag="las1mot",
 )
 
 laser_2_MOT = GaussianLaserBeam(
-    wavelength = 460.862e-9,
-    waist = (1e-2)*np.sqrt(2),
-    power = 3e-2,
-    waist_position = (0, 0, 0),
-    direction = (0, 0, -1),
-    polarization = CircularLeft(),
-    tag = "las2mot"    
+    wavelength=460.862e-9,
+    waist=(1e-2) * np.sqrt(2),
+    power=3e-2,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, -1),
+    polarization=CircularLeft(),
+    tag="las2mot",
 )
-
 
 
 # -- add everything to the configuration
@@ -66,13 +65,8 @@ config_1D_MOT.atom = atom_strontium
 config_1D_MOT += laser_1_MOT, laser_2_MOT, mag_field
 
 # setup atomlight
-config_1D_MOT.add_atomlight_coupling("las1", "main", -2*np.pi*12e6)
-config_1D_MOT.add_atomlight_coupling("las2", "main", -2*np.pi*12e6)
-
-
-
-
-
+config_1D_MOT.add_atomlight_coupling("las1", "main", -2 * np.pi * 12e6)
+config_1D_MOT.add_atomlight_coupling("las2", "main", -2 * np.pi * 12e6)
 
 
 # % GENERATE CONFIGURATION of the 1D molasse
@@ -88,23 +82,23 @@ main = atom_rubidium.trans["main"]
 # -- setup lasers of the 1D molasse
 # cf. config from Chen 2021
 laser_1_molasse = GaussianLaserBeam(
-    wavelength = 780.241e-9,
-    waist = (1e-2)*np.sqrt(2),
-    power = 1e-2,
-    waist_position = (0, 0, 0),
-    direction = (0, 0, 1),
-    polarization = CircularLeft(),
-    tag = "las1molasse"    
+    wavelength=780.241e-9,
+    waist=(1e-2) * np.sqrt(2),
+    power=1e-2,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, 1),
+    polarization=CircularLeft(),
+    tag="las1molasse",
 )
 
 laser_2_molasse = GaussianLaserBeam(
-    wavelength = 780.241e-9,
-    waist = (1e-2)*np.sqrt(2),
-    power = 1e-2,
-    waist_position = (0, 0, 0),
-    direction = (0, 0, -1),
-    polarization = CircularLeft(),
-    tag = "las2molasse"    
+    wavelength=780.241e-9,
+    waist=(1e-2) * np.sqrt(2),
+    power=1e-2,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, -1),
+    polarization=CircularLeft(),
+    tag="las2molasse",
 )
 
 
@@ -117,8 +111,5 @@ config_1D_molasse.atom = atom_rubidium
 config_1D_molasse += laser_1_molasse, laser_2_molasse
 
 # setup atomlight
-config_1D_molasse.add_atomlight_coupling("las1molasse", "main", -2*np.pi*6e6)
-config_1D_molasse.add_atomlight_coupling("las2molasse", "main", -2*np.pi*6e6)
-
-
-
+config_1D_molasse.add_atomlight_coupling("las1molasse", "main", -2 * np.pi * 6e6)
+config_1D_molasse.add_atomlight_coupling("las2molasse", "main", -2 * np.pi * 6e6)
