a07281e7221f689a80c237bb4e632347deb59ac2
Mateo Weill <mateo.weill@gmail.com>
2025-10-01 15:40:24 +0200

    added the 3D MOT to compare Doppler temp

--- src/atomsmltr/examples/chen2021.py
+++ src/atomsmltr/examples/chen2021.py
@@ -18,6 +18,8 @@ from atomsmltr.simulation import Configuration
 from atomsmltr.environment.lasers import CircularLeft, CircularRight
 from atomsmltr.environment.fields.magnetic import MagneticQuadrupoleZ
 
+# --------------------------------------------------------------------------------------------------------
+
 # % GENERATE CONFIGURATION of the 1D MOT
 
 
@@ -29,9 +31,11 @@ main = atom_strontium.trans["main"]
 
 # -- setup magnetic field: perfect quadrupole with a strong Z-axis in Chen 2021
 # Define magnet properties
-origin = np.array((0, 0, 0))
-gradient = 0.15  # T/m
-mag_field = MagneticQuadrupoleZ(origin=origin, slope=gradient, tag="mag_field_1")
+origin_1D = np.array((0, 0, 0))
+gradient_1D = 0.15  # T/m
+mag_field_1D = MagneticQuadrupoleZ(
+    origin=origin_1D, slope=gradient_1D, tag="mag_field_1D"
+)
 
 # -- setup lasers of the 1D MOT
 # cf. config from Chen 2021
@@ -62,12 +66,14 @@ config_1D_MOT = Configuration()
 config_1D_MOT.atom = atom_strontium
 
 # add objects
-config_1D_MOT += laser_1_MOT, laser_2_MOT, mag_field
+config_1D_MOT += laser_1_MOT, laser_2_MOT, mag_field_1D
 
 # setup atomlight
-config_1D_MOT.add_atomlight_coupling("las1", "main", -2 * np.pi * 12e6)
-config_1D_MOT.add_atomlight_coupling("las2", "main", -2 * np.pi * 12e6)
+config_1D_MOT.add_atomlight_coupling("las1mot", "main", -2 * np.pi * 12e6)
+config_1D_MOT.add_atomlight_coupling("las2mot", "main", -2 * np.pi * 12e6)
 
+
+# --------------------------------------------------------------------------------------------------------
 
 # % GENERATE CONFIGURATION of the 1D molasses
 
@@ -113,3 +119,109 @@ config_1D_molasses += laser_1_molasses, laser_2_molasses
 # setup atomlight
 config_1D_molasses.add_atomlight_coupling("las1molasses", "main", -2 * np.pi * 6e6)
 config_1D_molasses.add_atomlight_coupling("las2molasses", "main", -2 * np.pi * 6e6)
+
+
+# --------------------------------------------------------------------------------------------------------
+
+# % GENERATE CONFIGURATION of the 3D MOT
+
+
+# -- init config with strontium atom
+atom_rubidium = Rubidium()
+
+# -- get Strontium main transition information
+main = atom_rubidium.trans["main"]
+
+# -- setup magnetic field: perfect quadrupole with a strong Z-axis in Chen 2021
+# Define magnet properties
+origin_3D = np.array((0, 0, 0))
+gradient_3D = 18.2 * 1e-4  # T/m
+mag_field_3D = MagneticQuadrupoleZ(
+    origin=origin_3D, slope=gradient_3D, tag="mag_field_3D"
+)
+
+# -- setup lasers of the 1D MOT
+# cf. config from Chen 2021
+laser_1_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, 1),
+    polarization=CircularLeft(),
+    tag="las1mot3D",
+)
+
+laser_2_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(0, 0, -1),
+    polarization=CircularLeft(),
+    tag="las2mot3D",
+)
+
+laser_3_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(1, 0, 0),
+    polarization=CircularRight(),
+    tag="las3mot3D",
+)
+
+laser_4_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(-1, 0, 0),
+    polarization=CircularRight(),
+    tag="las4mot3D",
+)
+
+laser_5_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(0, 1, 0),
+    polarization=CircularRight(),
+    tag="las5mot3D",
+)
+laser_6_3D_MOT = GaussianLaserBeam(
+    wavelength=780.241e-9,
+    waist=66.7e-3,
+    power=0.02,
+    waist_position=(0, 0, 0),
+    direction=(0, -1, 0),
+    polarization=CircularRight(),
+    tag="las6mot3D",
+)
+
+
+# -- add everything to the configuration
+
+config_3D_MOT = Configuration()
+config_3D_MOT.atom = atom_rubidium
+
+# add objects
+config_3D_MOT += (
+    laser_1_3D_MOT,
+    laser_2_3D_MOT,
+    laser_3_3D_MOT,
+    laser_4_3D_MOT,
+    laser_5_3D_MOT,
+    laser_6_3D_MOT,
+    mag_field_3D,
+)
+
+# setup atomlight
+config_3D_MOT.add_atomlight_coupling("las1mot3D", "main", -2 * np.pi * 3e6)
+config_3D_MOT.add_atomlight_coupling("las2mot3D", "main", -2 * np.pi * 3e6)
+config_3D_MOT.add_atomlight_coupling("las3mot3D", "main", -2 * np.pi * 3e6)
+config_3D_MOT.add_atomlight_coupling("las4mot3D", "main", -2 * np.pi * 3e6)
+config_3D_MOT.add_atomlight_coupling("las5mot3D", "main", -2 * np.pi * 3e6)
+config_3D_MOT.add_atomlight_coupling("las6mot3D", "main", -2 * np.pi * 3e6)
