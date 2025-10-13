4cb92942325d2542703f252d97b471be21d6340e
Mateo Weill <mateo.weill@gmail.com>
2025-10-13 11:10:34 +0200

    needs to be fixed

--- src/atomsmltr/examples/config_1_1_1.py
+++ src/atomsmltr/examples/config_1_1_1.py
@@ -80,8 +80,7 @@ laser_1 = GaussianLaserBeam(
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(0.955, 0),
-    direction_type="thetaphi",
+    direction=(0, 0, 1),
     polarization=CircularLeft(),
     tag="las1",
 )
@@ -91,22 +90,17 @@ laser_2 = GaussianLaserBeam(
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(2.187, 0),
-    direction_type="thetaphi",
+    direction=(0, 0, -1),
     polarization=CircularLeft(),
     tag="las2",
 )
-
-print(laser_2.direction_type)
-print(laser_2.direction)
 
 laser_3 = GaussianLaserBeam(
     wavelength=780.241e-9,
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(0.955, -1.047),
-    direction_type="thetaphi",
+    direction=(1, 0, 0),
     polarization=CircularRight(),
     tag="las3",
 )
@@ -116,8 +110,7 @@ laser_4 = GaussianLaserBeam(
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(2.186, 2.0946),
-    direction_type="thetaphi",
+    direction=(-1, 0, 0),
     polarization=CircularRight(),
     tag="las4",
 )
@@ -127,8 +120,7 @@ laser_5 = GaussianLaserBeam(
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(0.955, 1.047),
-    direction_type="thetaphi",
+    direction=(0, 1, 0),
     polarization=CircularRight(),
     tag="las5",
 )
@@ -138,8 +130,7 @@ laser_6 = GaussianLaserBeam(
     waist=22e-3,
     power=100e-3 / 6,
     waist_position=(0, 0, 0),
-    direction=(2.186, -2.0946),
-    direction_type="thetaphi",
+    direction=(0, -1, 0),
     polarization=CircularRight(),
     tag="las6",
 )
@@ -150,6 +141,10 @@ config = Configuration()
 config.atom = atom
 config.add_objects([gravity])
 config += laser_1, laser_2, laser_3, laser_4, laser_5, laser_6
+
+
+# -- rotate the config to a (1,1,1)
+config.config_to_1_1_1()
 
 
 # -- setup lasers detuning parameters of the 3D MOT
@@ -163,7 +158,7 @@ list_lasers = config.list_lasers()
 for laser_name in list_lasers:
     laser = config.get_laser_copy(laser_name)
     direction = laser.direction
-    if direction[0] < np.pi / 2:
+    if direction[2] > 0:
         config.add_atomlight_coupling(laser_name, "main", detuning + epsilon)
     else:
         config.add_atomlight_coupling(laser_name, "main", detuning - epsilon)
